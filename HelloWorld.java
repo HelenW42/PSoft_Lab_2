1986dd0115d7cb5ae3b1b819011b112079313132
HelenW42 <hwilson3783351@gmail.com>
2025-09-10 19:27:42 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package edu.rpi.csci2600.f25.gildem4;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.printf("Farewell Sweet Earth");
     }
 }
